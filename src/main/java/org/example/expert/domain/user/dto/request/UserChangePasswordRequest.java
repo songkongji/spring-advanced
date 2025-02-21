27448be5115cbef5997040e9797523bcbcaa8e9d
songkongji <seungmin576@gmail.com>
2025-02-21 15:49:30 +0900

    level 1 - 3 validation UserService 의 changePassword 메서드 안에 request 의 내용을 검증하는 코드가 if 문으로 적혀있었다. 새 비밀번호가 8자 이상이며, 숫자와 대문자를 포함해야한다는 내용이었다. 이를 요청 Dto 에서 처리하기 위해 메서드 안 검증로직은 지워주었다. 그리고 그레이들에 validation 을 추가해주어 UserChangePasswordRequest 클래스에 @pattern 을 사용하여 정규표현식으로 해결하였다. 마지막으로 이를 적용하기 위해 UserController 의 changePassword 메소드의 파라미터 인 요청 dto 에 @Valid 를 적용헀다.

--- src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
+++ src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
@@ -1,6 +1,7 @@
 package org.example.expert.domain.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,6 +13,16 @@ public class UserChangePasswordRequest {
 
     @NotBlank
     private String oldPassword;
+
     @NotBlank
+    @Pattern(
+            regexp = "^(?=.*[A-Z])(?=.*\\d)[A-Za-z\\d]{8,}$",
+            message = "새 비밀번호는 8자 이상이어야 하고, 숫자와 대문자를 포함해야 합니다."
+    )
     private String newPassword;
 }
+// ^ : 문자열의 시작
+// (?=.*[A-Z]) : 적어도 하나의 대문자를 포함하는지 확인
+// (?=.*\d) : 적어도 하나의 숫자를 포함하는지 확인
+// [A-Za-z\d]{8,} : 대문자, 소문자, 숫자로 구성된 최소 8자 이상의 문자열
+// $ : 문자열의 끝
